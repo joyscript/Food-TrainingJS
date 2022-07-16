c7fe09bf66ce6fb95e814dc39ad747a8d2237b1a
joyscript <joyscript7@gmail.com>
2022-07-16 20:08:43 +0300

    add tabs.js

---
+++ js/tabs.js
@@ -0,0 +1,33 @@
+const tabcontents = document.querySelectorAll('.tabcontent');
+const tabsList = document.querySelector('.tabheader__items');
+const tabs = tabsList.querySelectorAll('.tabheader__item');
+
+const hideContent = () => {
+  tabcontents.forEach((content) => {
+    content.classList.remove('show', 'fade');
+    content.classList.add('hide');
+  });
+
+  tabs.forEach((tab) => tab.classList.remove('tabheader__item_active'));
+};
+
+const showContent = (i) => {
+  tabcontents[i].classList.remove('hide');
+  tabcontents[i].classList.add('show', 'fade');
+
+  tabs[i].classList.add('tabheader__item_active');
+};
+
+hideContent();
+showContent(0);
+
+tabsList.addEventListener('click', (e) => {
+  if (e.target.classList.contains('tabheader__item')) {
+    tabs.forEach((tab, i) => {
+      if (tab == e.target) {
+        hideContent();
+        showContent(i);
+      }
+    });
+  }
+});
