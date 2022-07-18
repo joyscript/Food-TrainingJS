81eebc3c127a3240f6c9630db2c0cdf3b247e7c2
joyscript <joyscript7@gmail.com>
2022-07-19 02:30:46 +0300

    refactor postData.js

--- js/postData.js
+++ js/postData.js
@@ -34,28 +34,29 @@ const postData = (form) => {
     const spinner = form.querySelector('.spinner');
     spinner.classList.add('show');
 
-    const request = new XMLHttpRequest();
-    request.open('POST', 'server.php');
-    request.setRequestHeader('Content-type', 'application/json');
-
     const formData = new FormData(form);
-    const obj = {};
-    formData.forEach((value, name) => (obj[name] = value));
-    const json = JSON.stringify(obj);
+    const dataObj = {};
+    formData.forEach((value, name) => (dataObj[name] = value));
 
-    request.send(json);
-
-    request.addEventListener('load', () => {
-      if (request.status === 200) {
-        console.log(request.response);
+    fetch('server.php', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(dataObj),
+    })
+      .then((data) => data.text())
+      .then((data) => {
+        console.log(data);
         showThanksModal(message.success);
-      } else {
+      })
+      .catch(() => {
         showThanksModal(message.failure);
-      }
-
-      form.reset();
-      spinner.classList.remove('show');
-    });
+      })
+      .finally(() => {
+        form.reset();
+        spinner.classList.remove('show');
+      });
   });
 };
 
