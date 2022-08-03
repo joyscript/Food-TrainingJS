88050f9f8a797aa9237f3eb327d6643492ee6459
joyscript <joyscript7@gmail.com>
2022-08-03 03:38:02 +0300

    improve timer function

--- js/timer.js
+++ js/timer.js
@@ -1,17 +1,34 @@
-const deadline = '2022-08-01T00:00:00';
+const timeBoxes = document.querySelectorAll('.timer span');
+const period = 7;
 
-const getTimeRemaining = (endtime) => {
-  const msec = Date.parse(endtime) - Date.now();
+let deadline = new Date('2022-08-08T00:00:00');
+
+const updateDeadline = () => {
+  if (deadline - Date.now() <= 0) {
+    deadline = new Date(Date.parse(deadline) + 1000 * 60 * 60 * 24 * period);
+    updateDeadline();
+  }
+};
+
+const updateDeadlineText = () => {
+  const months = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
+
+  document.querySelector('#end-day').textContent = deadline.getDate();
+  document.querySelector('#end-month').textContent = months[deadline.getMonth()];
+};
+
+const getTimeRemaining = () => {
+  const msec = deadline - Date.now();
 
   const days = Math.floor(msec / (1000 * 60 * 60 * 24));
   const hours = Math.floor((msec / (1000 * 60 * 60)) % 24);
   const minutes = Math.floor((msec / (1000 * 60)) % 60);
   const seconds = Math.floor((msec / 1000) % 60);
 
-  return [days, hours, minutes, seconds, msec];
+  return msec > 0 ? [days, hours, minutes, seconds, msec] : [0, 0, 0, 0, 0];
 };
 
-const normalizeNum = (num) => (num >= 0 && num < 10 ? `0${num}` : num);
+const normalizeNum = (num) => (num < 10 ? `0${num}` : num);
 
 const normalizeWord = (num) => {
   if (num % 10 === 1 && num !== 11) {
@@ -23,22 +40,18 @@ const normalizeWord = (num) => {
   }
 };
 
-const setTimer = (endtime) => {
-  const timeBoxes = document.querySelectorAll('.timer span');
+function updateTimer() {
+  const timeRem = getTimeRemaining();
+  const timer = setTimeout(updateTimer, 1000);
 
-  const updateTimer = () => {
-    const timeRem = getTimeRemaining(endtime);
+  if (timeRem[4] <= 1000) clearInterval(timer);
 
-    timeBoxes.forEach((timeBox, i) => {
-      timeBox.textContent = normalizeNum(timeRem[i]);
-      timeBox.nextSibling.textContent = normalizeWord(timeRem[i])[i];
-    });
+  timeBoxes.forEach((timeBox, i) => {
+    timeBox.textContent = normalizeNum(timeRem[i]);
+    timeBox.nextSibling.textContent = normalizeWord(timeRem[i])[i];
+  });
+}
 
-    if (timeRem[4] <= 1000) clearInterval(timeIterval);
-  };
-
-  updateTimer();
-  const timeIterval = setInterval(updateTimer, 1000);
-};
-
-setTimer(deadline);
+updateDeadline();
+updateDeadlineText();
+updateTimer();
