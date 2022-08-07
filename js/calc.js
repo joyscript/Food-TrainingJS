22ae60cde598981cedfde602fd51828cf428eaa2
joyscript <joyscript7@gmail.com>
2022-08-07 09:32:01 +0300

    refactor calc.js

--- js/calc.js
+++ js/calc.js
@@ -1,34 +1,16 @@
-const result = document.querySelector('.calculating__result > span');
-const clickFields = [document.querySelector('#gender'), document.querySelector('#level')];
-const inputField = document.querySelector('.calculating__choose_medium');
-const inputs = inputField.querySelectorAll('input');
+const calcField = document.querySelector('.calculating__field');
+const inputs = calcField.querySelectorAll('input');
+const result = calcField.querySelector('.calculating__result > span');
 
 const ratio = { low: 1.2, small: 1.375, medium: 1.55, high: 1.725 };
 const reg = /^[1-9]\d*|\d+\.(\d{1,2})$/;
 
 let user = { gender: 'female', level: 'small' };
 
-const activateElem = (field) => {
-  field.querySelectorAll('div').forEach((elem) => {
-    elem.classList.remove('calculating__choose-item_active');
-    if (elem.id == user[field.id]) elem.classList.add('calculating__choose-item_active');
-  });
-};
-
-const addClickHandlers = (field) => {
-  field.querySelectorAll('div').forEach((elem) => {
-    elem.addEventListener('click', () => {
-      user[field.id] = elem.id;
-      activateElem(field);
-      getResult();
-    });
-  });
-};
-
 const getResult = () => {
   const { gender, height, weight, age, level } = user;
 
-  if (weight && height && age && !inputField.querySelector('.attention')) {
+  if (weight && height && age && !calcField.querySelector('.attention')) {
     gender == 'female'
       ? (result.textContent = Math.round((447.6 + 9.2 * weight + 3.1 * height - 4.3 * age) * ratio[level]))
       : (result.textContent = Math.round((88.36 + 13.4 * weight + 4.8 * height - 5.7 * age) * ratio[level]));
@@ -37,17 +19,23 @@ const getResult = () => {
   }
 };
 
-// --------------------------------
-
 if (localStorage.getItem('user')) {
   user = JSON.parse(localStorage.getItem('user'));
   inputs.forEach((input) => (input.value = user[input.id]));
   getResult();
 }
 
-clickFields.forEach((field) => {
-  activateElem(field);
-  addClickHandlers(field);
+calcField.querySelector(`[data-gender="${user.gender}"]`).classList.add('active');
+calcField.querySelector(`[data-level="${user.level}"]`).classList.add('active');
+
+calcField.addEventListener('click', (e) => {
+  const btn = e.target;
+  if (btn.dataset.gender || btn.dataset.level) {
+    btn.dataset.gender ? (user.gender = btn.dataset.gender) : (user.level = btn.dataset.level);
+    btn.parentElement.querySelector('.active').classList.remove('active');
+    btn.classList.add('active');
+    getResult();
+  }
 });
 
 inputs.forEach((input) => {
